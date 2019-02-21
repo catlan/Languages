360ddf28afc96edb7743b804251660bdafeb458e
Graham Lee <leeg@labrary.online>
2019-02-21 12:15:06 +0000

    Demo of how to use breakpoints in the debugger.

--- LanguageKit/TracingDebugger/main.m
+++ LanguageKit/TracingDebugger/main.m
@@ -53,6 +53,14 @@ static LKAST *parseScript(NSString *script, NSString *extension)
     NSLog(@"Variables here: %@", [self.service allVariables]);
 }
 
+- (void)pause {
+    
+}
+
+- (void)resume {
+
+}
+
 @end
 
 @interface NSObject (AddedInScript)
@@ -82,9 +90,48 @@ int main(int argc, const char * argv[]) {
             exit(EX_SOFTWARE);
         }
         LKDebuggerService *debugger = [[LKDebuggerService alloc] init];
-        [debugger setMode:[TracingMode new]];
         [debugger debugScript:module];
+        
+        /*
+         * You can add a breakpoint by telling the debugger what AST node it ought to stop at.
+         * A UI for this debugger would either want to help users select nodes, or translate line
+         * numbers into nearest nodes to emulate instruction-based debuggers like gdb and lldb.
+         *
+         * You can break on any AST node, including comments! Here, I find the first statement
+         * in the first method in the first category defined in the script module, and break there.
+         */
+        LKAST *breakpoint = [[[(LKModule *)module allCategories][0] methods][0] statements][0];
+        NSLog(@"Breaking at statement: %@", breakpoint);
+        [debugger addBreakpoint:breakpoint];
+        /*
+         * When we run the interpreted method, the debugger will pause straight away. So the first
+         * thing to do is to set up another thread that tells it to resume after a timer, so that
+         * we can definitely continue.
+         */
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)),
+                       dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0),
+                       ^{
+                           NSLog(@"Resuming debugger");
+                           [debugger resume];
+                       });
+        /*
+         * Run the method! You shouldn't see anything for a few seconds, because the execution is
+         * paused in the debugger.
+         */
+        NSLog(@"Running in the debugger");
         NSLog(@"%@", [[NSObject new] doAThing]);
+        /*
+         * OK, we're done with that part of the demo, remove the breakpoint.
+         */
+        [debugger removeBreakpoint:breakpoint];
+        
+        /*
+         * Debugger users do not really need to write their own modes and set them on the debugger.
+         * This custom mode is used to show the information that's available to users of the debugger.
+         */
+        [debugger setMode:[TracingMode new]];
+        NSLog(@"%@", [[NSObject new] doAThing]);
+        
     }
     return 0;
 }
